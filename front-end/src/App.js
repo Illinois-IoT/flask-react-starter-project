ae30db7ac54cc78491331d6aeb9e5233d95f3149
Lily Li <jiayuli6@illinois.edu>
2022-06-24 09:56:18 -0500

    feat: init commit

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'http://10.195.118.144:5000/video_feed'} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
